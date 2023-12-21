6483cea2199f97aed3d25bceeab9790e3f5cb678
VadimRight <vadim.komashko@bk.ru>
2023-12-21 10:37:51 +0300

    Fit: get operation_type in html is done and working

--- src/main.py
+++ src/main.py
@@ -27,8 +27,7 @@ app = FastAPI(
     title="Trading App",
     lifespan=lifespan
 )
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
